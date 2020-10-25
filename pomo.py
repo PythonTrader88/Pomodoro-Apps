2024541a478bc983c4e53e63d544ea340e28d797
PythonTrader88 <PythonTrader88@gmail.com>
2020-10-25 15:57:50 -0400

    record_keeper module added.

--- pomo.py
+++ pomo.py
@@ -1,57 +1,63 @@
 """Pomodoro app main class"""
+
 import sys
 import os
-import json
-from datetime import datetime
+import csv
 import time
+import record_keeper
 
 class Pomodoro:
     def __init__(self):
         pass
         
+    def exit_program(self):
+        print("See you later!")    
+        sys.exit()
+        
     def run_timer(self):  #  Main loop for the program
         while True:
             #  Set how many minutes to count.
-            self.set_minutes = int(input("Enter Minutes: "))
-            self.begin_timer = input("Start(y/n): ")
-            
-            if self.begin_timer == 'n':
-                print('See you later!')
-                sys.exit()
+            self.set_minutes = input("Enter Minutes: ")
+            if self.set_minutes.lower() == 'q':
+                self.exit_program()    
+            try:
+                self.set_minutes = int(self.set_minutes)
+            except:
+                print("Must enter a number of minutes.\nEnter 'q' to quit.")
+                continue
+
+            self.begin_timer = input("Start(y/n): ")                        
+            if self.begin_timer.lower() == 'n' or self.begin_timer.lower() == 'q':
+                self.exit_program()
             
             self.timer(self.set_minutes, self.begin_timer)
 
-    def saving_result(self, result):
-        filename = 'records.json'
-        with open(filename, mode='a') as f:
-            
-            f.write(result)
-        
 
     def timer(self, set_minutes, begin_timer):
         if begin_timer.lower() == 'y':
             time_set = set_minutes * 60
             start_time = time.time()
+            local_start = time.localtime(start_time)
             end_time = start_time + time_set
+            
             
             while time.time() < end_time:
                 remaining_time = end_time - time.time()
-                print(f"{remaining_time // 60:.0f}:{round(remaining_time % 60):02}")
+                print(f"{remaining_time // 60:02.0f}:{round(remaining_time % 60):02}")
                 time.sleep(0.99999)
                 os.system("CLS")
                 
                 if remaining_time <= 1:
                     os.system("CLS")
-                    print("Complete!")
-                    date_stamp = time.strftime('%Y-%m-%d', time.localtime(start_time))
-                    start_time_literal = time.strftime('%I:%M %p')
-                    result = {date_stamp: (start_time_literal, set_minutes)}
-                    self.saving_result(str(result))
+                    date_stamp = time.strftime('%Y-%m-%d', local_start)
+                    started_time = time.strftime('%I:%M %p', local_start)
+                    record_keeper.saving_result(date_stamp, started_time, set_minutes)                
+                    print(
+                        f"Finished!\nCompleted {record_keeper.check_set_number(time.time())} pomodoro(s) today!"
+                                                       )
                 
         else:
-            print('See you later!')
-            sys.exit()
-
+            self.exit_program()
             
 if __name__ == '__main__':
     pom = Pomodoro()
---
+++ record_keeper.py
@@ -0,0 +1,46 @@
+"""Module for reading and writing records."""
+
+import csv
+import time
+import os
+
+def check_set_number(date):
+    filename = 'records.csv'
+    with open(filename, mode='r') as f:
+        count_today = 0
+        current_date = time.strftime('%Y-%m-%d', time.localtime(date))
+        reader = csv.DictReader(f, delimiter=',')
+        for row in reader:
+            if row['Date'] == current_date:
+                count_today += 1
+        
+        return count_today
+
+def saving_result(date, began_time,minutes):
+    filename = 'records.csv'
+    if not os.path.exists(filename):
+        with open(filename, mode='w') as f:
+            csv_writer = csv.DictWriter(f, fieldnames=['Date', 'Time_Finished', 'Minutes'])
+            csv_writer.writeheader()
+            
+    with open(filename, mode='a') as f:
+        csv_writer = csv.DictWriter(f, fieldnames=['Date', 'Time_Finished', 'Minutes'])
+        csv_writer.writerow({
+            'Date': date,
+            'Time_Finished': began_time,
+            'Minutes': minutes})
+
+
+# def check_set_number():
+#     filename = 'records.csv'
+#     with open(filename, mode='r') as f:
+#         values = defaultdict(int)
+#         current_date = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+#         reader = csv.DictReader(f, delimiter=',')
+#         for row in reader:
+#             date = datetime.strptime(row['Date'], '%Y-%m-%d')
+#             values[date.strftime('%Y-%m-%d')] += 1
+#             if row['Date'] == current_date:
+#                 count_today += 1
+        
+#         print(count_today)
