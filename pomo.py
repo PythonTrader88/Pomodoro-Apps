26d7b7559bb2b28ba009ecd8afe96910e07af5be
PythonTrader88 <PythonTrader88@gmail.com>
2020-10-26 13:29:42 -0400

    Refactor timer as a module and add short_rest.

--- pomo.py
+++ pomo.py
@@ -5,6 +5,7 @@ import os
 import csv
 import time
 import record_keeper
+import timer
 
 class Pomodoro:
     def __init__(self):
@@ -30,32 +31,9 @@ class Pomodoro:
             if self.begin_timer.lower() == 'n' or self.begin_timer.lower() == 'q':
                 self.exit_program()
             
-            self.timer(self.set_minutes, self.begin_timer)
+            timer.timer(self.set_minutes, self.begin_timer)
+            timer.short_rest()
 
-
-    def timer(self, set_minutes, begin_timer):
-        if begin_timer.lower() == 'y':
-            time_set = set_minutes * 60
-            start_time = time.time()
-            local_start = time.localtime(start_time)
-            end_time = start_time + time_set
-            
-            
-            while time.time() < end_time:
-                remaining_time = end_time - time.time()
-                print(f"{remaining_time // 60:02.0f}:{round(remaining_time % 60):02}")
-                time.sleep(0.99999)
-                os.system("CLS")
-                
-                if remaining_time <= 1:
-                    os.system("CLS")
-                    date_stamp = time.strftime('%Y-%m-%d', local_start)
-                    started_time = time.strftime('%I:%M %p', local_start)
-                    record_keeper.saving_result(date_stamp, started_time, set_minutes)                
-                    print(
-                        f"Finished!\nCompleted {record_keeper.check_set_number(time.time())} pomodoro(s) today!"
-                                                       )
-                
         else:
             self.exit_program()
             
